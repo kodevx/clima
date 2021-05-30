715eb0db850ca41a729ea176224453b827fdb24b
Rohith <rohithunni19@yahoo.com>
2021-05-30 19:48:49 +0530

    Constants Updates

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,12 @@
 export const DARK_MODE = 'dark';
 export const LIGHT_MODE = 'light';
+
+export const WEATHER_PARAMS = {
+    'Min Temp':'temp_min',
+    'Feels Like':'feels_like',
+    'Max Temp':'temp_max',
+    'Pressure':'pressure',
+    'Humidity':'humidity',
+    'Speed':'speed',
+    'Degree':'deg'
+};
